42ce576f92fbdaa0bbfef1d52db7c9d45d7274b0
Leonas <leonasl@gmail.com>
2017-06-08 18:10:42 -0700

    fix filename

--- lib/templates/missions/setup_host.sh
+++ lib/templates/missions/setup_host.sh
@@ -4,7 +4,7 @@
 
 source actions/install_basics.sh
 source actions/install_docker.sh
-source actions/install_firewall.sh
+source actions/configure_firewall.sh
 
 # https://github.com/v2tec/watchtower
 # Use watchtower to keep docker files auto updated
