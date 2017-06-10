c76049dc16317642852378cf115b185940a3bd4d
Leonas <leonasl@gmail.com>
2017-06-09 20:32:11 -0700

    works with example app

--- lib/templates/actions/start_app.sh
+++ lib/templates/actions/start_app.sh
@@ -1,3 +1,7 @@
 docker login -u="<%= @attributes.docker_username %>" -p="<%= @attributes.docker_password %>"
 docker pull <%= @attributes.app_repo %>
-docker run -d -p <%= @attributes.app_port_map %> --name=<%= @attributes.app_name %> --restart=always -d <%= @attributes.app_repo %>
+docker run --name=<%= @attributes.app_name %> \
+           --publish=<%= @attributes.app_port_map %> \
+           --restart=always \
+           --detach \
+           <%= @attributes.app_repo %>
--- lib/templates/missions/update_app.sh
+++ lib/templates/missions/update_app.sh
@@ -1,2 +1,3 @@
 docker stop <%= @attributes.app_name %>
+docker rm <%= @attributes.app_name %>
 source actions/start_app.sh
